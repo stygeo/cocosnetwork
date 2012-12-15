cb1c2170c48045d280054ecca0319fec65a079bc
Teh B3asts <dev@studyflow.nl>
2012-12-15 03:12:09 +0100

    hur

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,7 +1,7 @@
 (function() {
   var Game = {};
   Game.init = function(cb) {
-    var socket = io.connect('http://localhost:8080/')
+    var socket = io.connect('http://94.242.229.4:8080/')
 
     var director = cc.Director.sharedDirector
     director.attachInView(document.getElementById('cocos2d-demo'))
@@ -22,8 +22,17 @@
     cb.call(this, this);
   };
 
-  Game.addChar = function(character) {
+  Game.removeChild = function(child) {
+    this.layer.removeChild(child);
+  };
+
+  Game.addChild = function(character) {
+    character.net = this.net;
     this.layer.addChild(character);
+  };
+
+  Game.initChar = function(character) {
+    Game.addChild(character);
 
     var c = {id: character.name, pos: character.position};
     this.net.emit("init", c);
