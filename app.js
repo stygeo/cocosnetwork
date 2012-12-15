45e157f483dba78f3a37ea5327b24fb8c483516e
Jeffrey <stygeo@stygeo.bytesized-hosting.com>
2012-12-15 12:56:41 +0100

    update

--- app.js
+++ app.js
@@ -38,6 +38,13 @@ http.createServer(app).listen(app.get('port'), function(){
 var sockets = [];
 var characters = {};
 
+function emit(ev, cb) {
+  for(i in sockets) {
+    sockets[i].emit(ev, cb);
+  }
+}
+
+
 io.sockets.on('connection', function(socket) {
   var character;
   sockets.push(socket);
@@ -52,15 +59,19 @@ io.sockets.on('connection', function(socket) {
     character = data;
     characters[data.id] = data;
 
-    socket.emit('newCharacter', data);
+    for(c in characters) {
+      emit('newCharacter', characters[c]);
+    }
   });
 
-  socket.on('move', function(data) {
-    console.log(data);
+  socket.on('move', function(character) {
+    characters[character.id] = character;
+    emit('updateCharacter', character);
   });
 
   socket.on('disconnect', function() {
     console.log("Disconnected: "+character.id);
     delete characters[character.id];
+    emit('removeCharacter', character.id);
   });
 });
--- public/javascripts/character.js
+++ public/javascripts/character.js
@@ -4,9 +4,12 @@
     var size = director.winSize
 
     this.name = name;
+    this.net = undefined;
 
     Character.superclass.constructor.call(this, {url: 'images/grossini.png' });
-    this.position = new cc.Point(size.width / 2, size.height / 2)
+    var width = Math.round(Math.random() * 100);
+    var height = Math.round(Math.random() * 100);
+    this.position = new cc.Point((size.width / 2) + width, (size.height / 2) + height);
 
     return this;
   };
@@ -14,11 +17,16 @@
   Character.prototype = Object.create(cc.Sprite.prototype);
 
   Character.prototype.left =  function() {
+    this.position = new cc.Point(this.position.x - 10, this.position.y);
+    this.net.emit('move', {id: this.name, pos: this.position});
   };
+
   Character.prototype.right = function() {
   };
+
   Character.prototype.up = function() {
   };
+
   Character.prototype.down = function() {
   };
 
